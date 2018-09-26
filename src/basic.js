8da0d31fb3aba20fde9dcba3d1143d9daac7d181
Artsiom Patotski <potosik@gmail.com>
2018-09-27 00:14:02 +0300

    1-05 Redux: Writing a Counter Reducer

---
+++ src/basic.js
@@ -0,0 +1,11 @@
+// counter reducer
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+         defeault:
+            return state;
+    }
+}
