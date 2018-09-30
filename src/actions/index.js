ff3693a6ce7cbc1c49fd579285bc73a22f1a0180
Artsiom Patotski <potosik@gmail.com>
2018-09-30 15:38:25 +0300

    2-03 Redux: Persisting the state to the local storage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import {v4} from 'node-uuid';
+
 // ACTION CREATORS
 let nextTodoId = 0;
 export const addTodo = (text) => ({
     type: 'ADD_TODO',
-    id: nextTodoId++,
+    id: v4(),
     text: text
 });
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import todoApp from './reducers';
 import App from './components/App';
+import {loadState, saveState} from './localStorage';
+import throttle from 'lodash/throttle';
 
-const persistedState = {
-    todos: [{
-        id: '0',
-        text: 'Welcome back!',
-        completed: false
-    }]
-};
+const persistedState = loadState();
 
 const store = createStore(todoApp, persistedState);
 registerServiceWorker();
 
+// subscribe to changes to save state to local storage
+store.subscribe(throttle(() => {
+    // save only data state, not UI part
+    saveState({
+        todos: store.getState().todos
+    });
+}, 1000));
+
 // inject store as a parameter for top level component
 render(
     <Provider store={store}>
-        <App />
+        <App/>
     </Provider>,
     document.getElementById('root')
 );
