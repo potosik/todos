a97882292e6a40a3df6c523aadbeb6f0c25bd639
Artsiom Patotski <apatotski@scnsoft.com>
2018-10-01 16:40:49 +0300

    2-18 Redux: Applying redux middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,75 +1,24 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import promise from 'redux-promise';
+import logger from 'redux-logger';
 import todoApp from "./reducers";
-
-const legger = (store) => (next) => {
-    if (!console.group) {
-        return next;
-    }
-
-    return (action) => {
-        console.group(action.type);
-        console.log('%c prev state', 'color: gray', store.getState());
-        console.log('%c action', 'color: blue', action);
-        const returnValue = next(action);
-        console.log('%c next state', 'color: green', store.getState());
-        console.groupEnd(action.type);
-        return returnValue;
-    }
-};
-
-const promise = (store) => (next) => (action) => {
-    // check if action is a promise
-    if (typeof action.then === 'function') {
-        // if it is wait for resolve and then path it to original dispatch
-        return action.then(next);
-    }
-    return next(action);
-};
-
-/*
-const addPromiseSupportToDispatch = (store) => {
-    return (next) => {
-        return (action) => {
-            // check if action is a promise
-            if (typeof action.then === 'function') {
-                // if it is wait for resolve and then path it to original dispatch
-                return action.then(next);
-            }
-            return next(action);
-        }
-    }
-};
-
-const addPromiseSupportToDispatch = (store) => {
-    const next = store.dispatch;
-    return (action) => {
-        // check if action is a promise
-        if (typeof action.then === 'function') {
-            // if it is wait for resolve and then path it to original dispatch
-            return action.then(next);
-        }
-        return next(action);
-    }
-};
- */
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-    middlewares.slice().reverse().forEach(middleware =>
-        store.dispatch = middleware(store)(store.dispatch)
-    );
-};
 
 // order of dispatch modification is very important!
 const configureStore = () => {
-    const store = createStore(todoApp);
     const middlewares = [promise];
-
     if (process.env.NODE_ENV !== 'production') {
-        middlewares.push(legger);
+        middlewares.push(logger);
     }
 
-    wrapDispatchWithMiddlewares(store, middlewares);
-
+    const store = createStore(
+        todoApp,
+        /*
+            persisited state should be the second parameter, but it's optional
+            persistedState,
+        */
+        // applying middleware
+        applyMiddleware(...middlewares)
+    );
     return store;
 };
 
