6301c4f2cc5dea759474b1f76f6154e572d7ba26
Artsiom Patotski <apatotski@scnsoft.com>
2018-10-01 14:42:09 +0300

    2-14 Redux: Fetching data on route change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,22 +1,51 @@
+import React from 'react';
 import {connect} from 'react-redux';
 import {toggleTodo} from '../actions';
 import {withRouter} from 'react-router';
 import {getVisibleTodos} from '../reducers';
 import TodoList from './TodoList';
+import {fetchTodos} from "../api";
+
+class VisibleTodoList extends React.Component {
+    componentDidMount() {
+        // getting filter property from mounted props by mapStateToProps function
+        fetchTodos(this.props.filter).then(todos =>
+            console.log(this.props.filter, todos)
+        );
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.filter !== prevProps.filter) {
+            // getting filter property from mounted props by mapStateToProps function
+            fetchTodos(this.props.filter).then(todos =>
+                console.log(this.props.filter, todos)
+            );
+        }
+    }
+
+    render() {
+        return <TodoList {...this.props}/>;
+    }
+}
 
 // returns props that is depends on store
 // maps store state to the props of component
-const mapStateToProps = (state, {match}) => ({
-    // get filter from match.params by router
-    // 'all' as default
-    todos: getVisibleTodos(state, match.params.filter || 'all')
-});
+const mapStateToProps = (state, {match}) => {
+    const filter = match.params.filter || 'all';
+    return {
+        // get filter from match.params by router
+        // 'all' as default
+        todos: getVisibleTodos(state, filter),
+        // to get filter inside of VisibleTodoList component created above
+        filter
+    }
+};
 // auto-generated container component for mapping state and
 // dispatch function to target component
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList = withRouter(connect(
     mapStateToProps,
-    { onTodoClick: toggleTodo }
+    {onTodoClick: toggleTodo}
     // passing target component as a parameter for function
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import {render} from 'react-dom';
 import Root from './components/Root';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './configureStore';
-import {fetchTodos} from "./api";
-
-fetchTodos('all').then(todos =>
-    console.log(todos)
-);
 
 const store = configureStore();
 registerServiceWorker();
