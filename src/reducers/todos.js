c4bbd7a4d42d6ba0029552ffaa26529cf66dfbd7
Artsiom Patotski <apatotski@scnsoft.com>
2018-10-01 14:06:26 +0300

    2-11 Redux: Normalizing the state shape

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,58 +1,49 @@
-// its called: reducer composition
-// each reducer passes only a part of state tree when calls other reducer
-const todo = (state, action) => {
-    switch (action.type) {
-        // just return a new object
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            };
-        // operation on concrete todo element
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
+import {combineReducers} from 'redux';
+import todo from './todo'
 
-            // copy an object and replace one field
+const byId = (state = {}, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+        case 'TOGGLE_TODO':
             return {
                 ...state,
-                completed: !state.completed
+                // making changes on object by id
+                // passing current state of the target object and an action
+                [action.id]: todo(state[action.id], action)
             };
         default:
             return state;
     }
 };
 
-const todos = (state = [], action) => {
+const allIds = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
-            return [
-                ...state,
-                // will create a new object and add it to array
-                todo(undefined, action)
-            ];
-        case 'TOGGLE_TODO':
-            // just perform an operation on every element
-            return state.map(t => todo(t, action));
+            return [...state, action.id];
         default:
             return state;
     }
 };
+
+const todos = combineReducers({byId, allIds});
 
 export default todos;
+
+const getAllTodos = (state) =>
+    state.allIds.map(id => state.byId[id]);
 
 // helper to filter available todos
 // the selector function
 // selects something from the corresponding state
+// state now contains state for byId and allIds reducers!!!
 export const getVisibleTodos = (state, filter) => {
+    const allTodos = getAllTodos(state);
     switch (filter) {
         case 'all':
-            return state;
+            return allTodos;
         case 'active':
-            return state.filter(t => !t.completed);
+            return allTodos.filter(t => !t.completed);
         case 'completed':
-            return state.filter(t => t.completed);
+            return allTodos.filter(t => t.completed);
     }
 };
